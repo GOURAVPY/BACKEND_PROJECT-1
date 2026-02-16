1c5a9e04f69cf02a576e772741b7b9645114a8d1
GOURAVPY <gouravsuman2006@gmail.com>
2026-02-16 18:59:36 +0530

    controlas

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -1,9 +1,65 @@
+import { Apierror } from '../utils/apiError.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
+import { User } from '../models/user.models.js';
+import { uploadOnCloudinary } from '../utils/filleUplode.js';
 
 const registerUser = asyncHandler(async (req, res) => {
-  return res.status(200).json({
-    message: 'its ok',
+  //get user data from frantend
+  //validation - not empty
+  const { fullname, email, username, password } = req.body;
+  console.log('email', email);
+
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === '')
+  ) {
+    throw new Apierror(400, 'all fields are required');
+  }
+
+  //check is user aleady exists usename, email
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if (existedUser) {
+    throw new Apierror(409, 'user whit email or username already exists');
+  }
+  //check for images, check for avatar
+
+  const avatarLocalpath = req.files?.avatar[0]?.path;
+  const coverImageLocalpath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalpath) {
+    throw new Apierror(400, 'avatar is required');
+  }
+
+  //upload them to cloudinary ,avatar
+  const avatar = await uploadOnCloudinary(avatarLocalpath);
+  const coverimage = await uploadOnCloudinary(coverImageLocalpath);
+
+  if (!avatar) {
+    throw new Apierror(400, 'avatar is required');
+  }
+  //create user object - create entry in db
+  const user = await User.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || '',
+    email,
+    password,
+    username: username.toLowercase(),
+  });
+
+ const idusercreated = await User.findById(user._id).select(
+  "-password -refreshToken"
+ )
+ if(!idusercreated){
+  throw new Apierror(500,"Something want wrong while registrig the user ");
+  
+ }
+  //remove passward and refresh token from response
+  //chech for user creation
+  //return res
 });
 
-export {registerUser}
+export { registerUser };
