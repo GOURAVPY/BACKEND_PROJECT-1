96925d5f6cfd7b130705409a2427282917a80c43
GOURAVPY <gouravsuman2006@gmail.com>
2026-02-16 16:59:54 +0530

    controller. or routes

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ app.use(
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
-app.use(cookieParser())
+app.use(cookieParser());
 
+import userRoutes from './routes/User.routes.js';
+
+app.use('/api/v1/users', userRoutes);
 export { app };
